65efb7d1a4b44c5bda74ce8a6f237bf5174e217b
Nik <nik.machula@yandex.ru>
2018-01-15 22:56:13 +0400

    Olimp parsing refactoring. Added handicap, total, individual totals parsing.

--- surebet/parsing/olimp.py
+++ surebet/parsing/olimp.py
@@ -1,13 +1,21 @@
 import lxml.html
+import re
 
 from surebet.parsing import *
 from surebet.parsing.bets import *
 
 # table_xpath = '//*[@id="champ_container_"]/table/tr[position() mod 2 = 0]/td/table'
-table_xpath = '//*[@id="champ_container_"]/table/tbody/tr'
+BLOCKS_XPATH = '//*[@id="champ_container_"]/table/tr'
+BLOCK_BODY_ROWS_XPATH = 'td/table/tr'
+RAW_SPORT_NAME_XPATH = 'td[1]/text()'
+TEAMS_STR_XPATH = 'td[2]/font/b/span/text()'
+MAIN_BETS_ROWS_XPATH = 'td/div/nobr'
+OTHER_BETS_XPATH = 'td/div/div/*'
 
-# bet_types = ['Tot', 'H2', 'H1', 'X2', '12', '1X', '2', 'X', '1']
-bet_types = ['X2', '12', '1X', '2', 'X', '1']  # ['Tot', 'H2', 'H1']
+BET_TYPES = ['X2', '12', '1X', '2', 'X', '1']  # ['Tot', 'H2', 'H1']
+
+DOUBLE_PARENTHESES_REGEXP = r'^.*\(.*\) - .*\(.*\).*$'
+PARENTHESES_REGEXP = r'^.*\(.*\).*$'
 
 
 def parse(source):
@@ -22,79 +30,124 @@ def parse(source):
 
     html = lxml.html.fromstring(source)
 
-    trs = xpath_with_check(html, table_xpath)
-    if len(trs) % 2 != 0:
-        raise ParseException('count of tr elements in {} should be even'.format(table_xpath))
+    rows = xpath_with_check(html, BLOCKS_XPATH)
+    if len(rows) % 2 != 0:
+        raise ParseException('count of tr elements in {} should be even'.format(BLOCKS_XPATH))
 
     blocks = []
-    for i in range(len(trs) // 2):
-        blocks.append((trs[i * 2], trs[i * 2 + 1]))
+    for i in range(len(rows) // 2):
+        blocks.append((rows[i * 2], rows[i * 2 + 1]))
 
-    for block in blocks:
+    for block_head, block_body in blocks:
         # each block has same sport name
-        raw_sport_name = ''.join(xpath_with_check(block[0], 'td[1]/text()'))
+        raw_sport_name = ''.join(xpath_with_check(block_head, RAW_SPORT_NAME_XPATH))
         sport_name = raw_sport_name.partition('.')[0].strip()
+        if sport_name not in bookmaker_sports:
+            continue
 
-        rows = xpath_with_check(block[1], 'td/table/tbody/tr')
-        if len(rows) % 2 != 0:
-            raise ParseException('count of tr elements in {} should be even'.format('td/table/tbody/tr'))
+        block_body_rows = xpath_with_check(block_body, BLOCK_BODY_ROWS_XPATH)
+        if len(block_body_rows) % 2 != 0:
+            raise ParseException('count of tr elements in {} should be even'.format(BLOCK_BODY_ROWS_XPATH))
 
-        for i in range(len(rows) // 2):
+        for i in range(len(block_body_rows) // 2):
             # current event
-            teams_row, coefs_row = rows[i * 2], rows[i * 2 + 1]
-            teams_str = ''.join(xpath_with_check(teams_row, 'td[2]/font/b/span/text()'))  # teams
-            # deleting prefix digits and punct
-            teams_str = delete_prefix_digits_and_punct(teams_str.strip())
-            teams_str = teams_str.strip()
-            teams = teams_str.split(' - ')
+            teams_row, coefficients_row = block_body_rows[i * 2], block_body_rows[i * 2 + 1]
 
-            # team 1
-            first_team = teams[0].strip()
-            # team 2
-            second_team = teams[1].strip()
+            teams_str = ''.join(xpath_with_check(teams_row, TEAMS_STR_XPATH))
+            first_team, second_team = get_teams(teams_str)
 
             # getting coefficients
-            # main bets
-            nobrs = coefs_row.xpath('td/div/nobr')  # nobrs may be []
-            # nobrs = xpath_with_check(coefs_row, 'td/div/nobr')
             parts = []
-            bets = get_main_coefs(nobrs)
+            # main bets
+            main_bets_rows = coefficients_row.xpath(MAIN_BETS_ROWS_XPATH)  # main_bets_rows may be []
+            bets = get_main_bets(main_bets_rows)
+            # other bets
+            other_bets_rows = coefficients_row.xpath(OTHER_BETS_XPATH)
+            bets.ind_total1, bets.ind_total2 = get_individual_totals(other_bets_rows, first_team, second_team)
+
             parts.append(bets)
 
-            event = Event(first_team, second_team, parts)
-            # adding to bookmaker
-            if sport_name in bookmaker_sports:
-                bookmaker_sports[sport_name].append(event)
-
-    # for k, v in bookmaker_sports.items():
-    #     print(k, '({}) :'.format(len(v)), v)
+            bookmaker_sports[sport_name].append(Event(first_team, second_team, parts))
 
     return bookmaker
 
 
-def get_main_coefs(nobrs):
-    # if not nobrs:
+def get_main_bets(main_bets_rows):
+    # if main_bets_rows == []:
     #     print('suspended')
-    #  self.o1, self.ox, self.o2, self.o1x, self.o12, self.ox2
     # ['o1', 'ox', 'o2', 'o1x', 'o12', 'ox2']
     bets = PartBets()
-    for nobr in nobrs:
-        nobr_text = nobr.text.strip()
-        nobr_text = nobr_text.partition(' ')[0].partition('(')[0]
+    for row in main_bets_rows:
+        bet_type = row.text.strip().partition(' ')[0].partition('(')[0]
+        span = xpath_with_check(row, 'span')[0]
 
-        if nobr_text in bet_types:
-            attribute_name = 'o' + nobr_text.lower()
-            coefficient = nobr.xpath('span')[0].get('data-v1')
-            set_exist_attr(bets, attribute_name, parse_factor(coefficient))
+        # X2, 12, 1X, 2, X, 1
+        if bet_type in BET_TYPES:
+            attribute_name = 'o' + bet_type.lower()
+            set_exist_attr(bets, attribute_name, parse_factor(span.get('data-v1')))
+
+        # Total
+        if bet_type == 'Tot':
+            bets.total.append(CondBet(parse_factor(span.get('data-v1')),
+                                      parse_factor(span.get('data-v3')),
+                                      parse_factor(span.get('data-v2'))))
+
+        # Handicap
+        if bet_type == 'H1':
+            bets.hand.append(CondBet(parse_factor(span.get('data-v1')),
+                                     parse_factor(span.get('data-v2')),
+                                     0.0))
+
+        if bet_type == 'H2':
+            if bets.hand:
+                bets.hand[0].v2 = parse_factor(span.get('data-v2'))
 
     return bets
 
 
-def delete_prefix_digits_and_punct(string):
-    if not string:
-        raise Exception('empty string')
-    i = 0
-    while i < len(string) and string[i].isdigit() or string[i] in '.,;:%#$^&*~`<>/?{}[]-+|':
-        i += 1
-    return string[i:]
+def get_individual_totals(other_bets_rows, first_team, second_team):
+    ind_total1, ind_total2 = [], []
+    bet_type = ''
+    for row in other_bets_rows:
+        if row.tag == 'b':
+            bet_type = xpath_with_check(row, 'i')[0].text.strip()
+        if row.tag == 'nobr':
+            if bet_type == 'Individual total:':
+                team = row.text.rpartition('(')[0].strip()
+                span = xpath_with_check(row, 'span')[0]
+                cond_bet = CondBet(parse_factor(span.get('data-v1')),
+                                   parse_factor(span.get('data-v2')),
+                                   parse_factor(span.get('data-v3')))
+                if team == first_team:
+                    ind_total1.append(cond_bet)
+                elif team == second_team:
+                    ind_total2.append(cond_bet)
+                else:
+                    raise ParseException('Undefined team "{}". First: "{}". Second: "{}"'.format(team, first_team,
+                                                                                                 second_team))
 
+    return ind_total1, ind_total2
+
+
+def get_teams(teams_str):
+    # '  International - RS (U19) - EC XV De Novembro Jau SP (U19)'
+    teams_str = delete_prefix_number(teams_str.strip())
+    teams_str_parts = tuple(map(lambda s: s.strip(), teams_str.split(' - ')))
+    if len(teams_str_parts) == 2:
+        return teams_str_parts
+
+    matches_double_parentheses = bool(re.match(DOUBLE_PARENTHESES_REGEXP, teams_str))  # *(*) - *(*)
+    if matches_double_parentheses:
+        for i in range(1, len(teams_str_parts)):
+            first_team, second_team = ' - '.join(teams_str_parts[:i]), ' - '.join(teams_str_parts[i:])
+            if re.match(PARENTHESES_REGEXP, first_team) and re.match(PARENTHESES_REGEXP, second_team):
+                return first_team, second_team
+
+    raise ParseException('Teams string parsing error. Raw : "{}". Handled: "{}".'.format(teams_str, teams_str_parts))
+
+
+def delete_prefix_number(string):
+    if not string:
+        raise ParseException('Empty string')
+    result = re.match(r'^[0-9]+\.', string)
+    return string[result.end():] if result else string
--- surebet/tests/parsing/test_fonbet.py
+++ surebet/tests/parsing/test_fonbet.py
@@ -16,7 +16,7 @@ def abs_path(filename):
 def test_samples():
     for num in range(3):
         filename = abs_path('sample{}.html'.format(num))
-        with open(filename) as file:
+        with open(filename, encoding='utf-8') as file:
             html = file.read()
         parse(html)
         logging.info('PASS: sample{}'.format(num))
@@ -48,3 +48,4 @@ def test_broken_structure():
         parse(html)
 
     logging.info('PASS: broken structure')
+
