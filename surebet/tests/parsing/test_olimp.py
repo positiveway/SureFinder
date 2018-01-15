24ee518f9796b0515dd25fa51ceab40628035b87
Nik <nik.machula@yandex.ru>
2018-01-15 23:09:29 +0400

    test_olimp.py cleanup

--- surebet/tests/parsing/test_olimp.py
+++ surebet/tests/parsing/test_olimp.py
@@ -16,7 +16,7 @@ def abs_path(filename):
 def test_samples():
     for num in range(3):
         filename = abs_path('sample{}.html'.format(num))
-        with open(filename, encoding='utf-8') as file:
+        with open(filename) as file:
             html = file.read()
         parse(html)
         logging.info('PASS: sample{}'.format(num))
@@ -24,11 +24,11 @@ def test_samples():
 
 def test_known_result():
     filename = abs_path('knownResult.json')
-    with open(filename, encoding='utf-8') as file:
+    with open(filename) as file:
         known_result = json.load(file)
 
     filename = abs_path('knownResult.html')
-    with open(filename, encoding='utf-8') as file:
+    with open(filename) as file:
         html = file.read()
 
     olimp = parse(html)
@@ -41,25 +41,10 @@ def test_known_result():
 
 def test_broken_structure():
     filename = abs_path('brokenStructure.html')
-    with open(filename, encoding='utf-8') as file:
+    with open(filename) as file:
         html = file.read()
 
     with pytest.raises(ParseException, message='Expecting ParseException'):
         parse(html)
 
     logging.info('PASS: broken structure')
-
-if __name__ == '__main__':
-    # test_samples()
-    # test_broken_structure()
-    test_known_result()
-
-    # filename = abs_path('knownResult.html')
-    # with open(filename, encoding='utf-8') as file:
-    #     html = file.read()
-    #
-    # olimp = parse(html)
-    # olimp.format()
-    #
-    # with open(abs_path('knownResult.json'), 'w', encoding='utf-8') as json_file:
-    #     json_file.write(obj_to_json(olimp))
