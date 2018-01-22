ced0e4d49fb054df6f0982ac5a7fd8259e0824d2
Max <max52853@gmail.com>
2018-01-22 20:46:27 +0300

    Some changes in posit.py for checking...

--- surebet/loading/posit.py
+++ surebet/loading/posit.py
@@ -1,23 +1,23 @@
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from surebet.loading import *
+
+
+name = "positivebet"
+url = "http://positivebet.com/en"
 
 
 def log_in(browser, account):
-    browser.get("http://positivebet.com/en")
+    browser.get(url)
     browser.find_element_by_link_text("Login").send_keys(Keys.RETURN)
     browser.find_element_by_id("UserLogin_username").send_keys(account['login'])
     browser.find_element_by_id("UserLogin_password").send_keys(account['password'])
     browser.find_element_by_name("yt0").send_keys(Keys.RETURN)
-    # after logging into account we're redirecting on the main page and then...
-    # seems like the button "Home" (on the site) is pressing by site right after main page was loaded,
-    # so if we will try to click on "Live bets" button right after main page is loaded, we will do this, ok,
-    # but right after this site clicks on "Home" button and we stay on main page
-    # that's why next command is used 2 times (or may be we should just wait for something?)
     browser.find_element_by_link_text("Live bets").send_keys(Keys.RETURN)
     browser.find_element_by_link_text("Live bets").send_keys(Keys.RETURN)
     browser.find_element_by_name("yt1").send_keys(Keys.RETURN)
-    # in the process...
-    browser.close()
+    browser.find_element_by_id("ddlPerPage").send_keys(Keys.RETURN)
+    browser.find_element_by_xpath("//select[@name='Bets[per_page]']/option[text()='30']").click()
+    log_loaded(name)
 
 
-log_in(webdriver.Chrome(), {'login' : "kolyan312@gmail.com", 'password' : "1q1w1e1r"})
+# log_in(webdriver.Chrome(), {'login' : "kolyan312@gmail.com", 'password' : "1q1w1e1r"})
