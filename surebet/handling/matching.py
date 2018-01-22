c7c6f09bdef18da2aea3cae2cfca36711be51270
marshygeek <lester0578@gmail.com>
2018-01-23 00:47:37 +0400

    Added positive parsing and testing. Some refactorings

--- surebet/handling/matching.py
+++ surebet/handling/matching.py
@@ -8,27 +8,27 @@ MATCH_RATIO = 80
 def match_sports(sport1, sport2):
     for event1 in sport1:
         for event2 in sport2:
-            reversed_teams = match_events(event1, event2)
+            reversed_teams = _match_events(event1, event2)
             if reversed_teams is not None:
                 yield MatchedEventPair(event1, event2, reversed_teams)
 
 
-def join_teams(team1, team2):
+def _join_teams(team1, team2):
     return " ".join((team1, team2))
 
 
-def match_events(event1, event2):
+def _match_events(event1, event2):
     reversed_teams = None
 
     teams1 = (event1.team1, event1.team2)
     teams2 = (event2.team1, event2.team2)
-    if is_equal(join_teams(*teams1), join_teams(*teams2)):
+    if _is_equal(_join_teams(*teams1), _join_teams(*teams2)):
         reversed_teams = False
-    elif is_equal(join_teams(*teams1), join_teams(*reversed(teams2))):
+    elif _is_equal(_join_teams(*teams1), _join_teams(*reversed(teams2))):
         reversed_teams = True
 
     return reversed_teams
 
 
-def is_equal(str1, str2):
+def _is_equal(str1, str2):
     return fuzz.ratio(str1, str2) > MATCH_RATIO
---
+++ surebet/parsing/posit.py
@@ -0,0 +1,155 @@
+from itertools import combinations
+from lxml import html
+from re import match
+
+from surebet.handling import *
+from surebet.parsing import *
+
+book_names = ["fonbet", "marat", "olimp"]
+
+xp_rows = '//table/tbody/tr[not(@id="")]'
+none_factor = 0
+
+
+def parse(source):
+    all_surebets = {}
+    for book1_name, book2_name in combinations(book_names, 2):
+        all_surebets[(book1_name, book2_name)] = Surebets(book1_name, book2_name)
+
+    doc = html.fromstring(source)
+    for row in xpath_with_check(doc, xp_rows):
+        cols = xpath_with_check(row, "./td")
+
+        book_pair = _get_book_pair(cols[2])
+        is_reversed = book_names.index(book_pair[0]) > book_names.index(book_pair[1])
+        if is_reversed:
+            book_pair = tuple(reversed(book_pair))
+
+        surebet = _get_surebet(cols[4], is_reversed)
+        if not surebet:
+            continue
+
+        sport_name = _get_sport_name(cols[0])
+        sport = getattr(all_surebets[book_pair], sport_name)
+
+        events_teams = _get_events_teams(cols[3], is_reversed)
+        e_surebets = next((ev for ev in sport if ev.teams1 == events_teams[0] and ev.teams2 == events_teams[1]), None)
+        if not e_surebets:
+            e_surebets = EventSurebets(*events_teams)
+            sport.append(e_surebets)
+
+        part_num = _get_part_num(cols[4])
+        part_surebets = next((part for part in e_surebets.parts if part.part == part_num), None)
+        if not part_surebets:
+            part_surebets = PartSurebets([], part_num)
+            e_surebets.parts.append(part_surebets)
+
+        part_surebets.surebets.append(surebet)
+
+    return list(all_surebets.values())
+
+
+def _get_sport_name(node):
+    raw_name = xpath_with_check(node, "./a[not(@data-delay)]/img")[0].get("alt")
+    return {
+        "Soccer": "soccer",
+        "Hockey": "hockey",
+        "Basketball": "basket",
+        "Tennis": "tennis",
+        "Volleyball": "volley",
+    }[raw_name]
+
+
+def _get_book_pair(node):
+    posit_names = {
+        "fonbet": "fonbet",
+        "marathon": "marat",
+        "olimp": "olimp",
+    }
+
+    book1_name = get_text(xpath_with_check(node, "./a[1]")[0])
+    book2_name = get_text(xpath_with_check(node, "./a[2]")[0])
+    return posit_names[book1_name], posit_names[book2_name]
+
+
+def _get_events_teams(node, is_reversed):
+    name_nodes = xpath_with_check(node, './a[not(@rel="tooltip")]')
+    if is_reversed:
+        name_nodes = list(reversed(name_nodes))
+
+    events_teams = []
+    for name_node in name_nodes:
+        name = get_text(name_node)
+        events_teams.append(parse_teams(name, " vs "))
+
+    return events_teams
+
+
+def _get_part_num(node):
+    part_num = 0
+    node_text = get_text(xpath_with_check(node, "./nobr/a")[0])
+
+    res = search(r"\((\d) [a-z]+\)", node_text)
+    if res:
+        part_num = int(res.group(1))
+    return part_num
+
+
+def _get_surebet(node, is_reversed):
+    stake_nodes = xpath_with_check(node, "./nobr/a")
+    if is_reversed:
+        stake_nodes = list(reversed(stake_nodes))
+
+    excluded_name = "inc OT"
+    res_bet_patterns = {
+        r"Win 1.*": "o1",
+        r"1[^2].*": "o1",
+        r"Win 2.*": "o2",
+        r"2.*": "o2",
+        r"X[^2].*": "ox",
+        r"1X.*": "o1x",
+        r"X2.*": "ox2",
+        r"12.*": "o12",
+    }
+    cond_bet_patterns = {
+        r"H(\d)\(([^\)]+)\).*": "hand",  # H1(1.5)
+        r"(Under|Over)\(([^\)]+)\).*": "total",  # Under(1.5)
+        r"T1 (Under|Over)\(([^\)]+)\).*": "ind_total1",  # T1 Under(1.5)
+        r"T2 (Under|Over)\(([^\)]+)\).*": "ind_total2",  # T2 Under(1.5)
+    }
+    wagers = []
+    for wager_num in range(2):
+        stake_name = get_text(stake_nodes[wager_num]) + "$"  # adding $ for proper regexp matching
+        if excluded_name in stake_name:
+            continue
+
+        wager_name = None
+        for res_bet_pattern in res_bet_patterns:
+            if match(res_bet_pattern, stake_name):
+                wager_name = res_bet_patterns[res_bet_pattern]
+                break
+        if wager_name:
+            wagers.append(Wager(wager_name, none_factor))
+            continue
+
+        found_wager = None
+        for cond_bet_pattern in cond_bet_patterns:
+            found_wager = match(cond_bet_pattern, stake_name)
+            if found_wager:
+                wager_name = cond_bet_patterns[cond_bet_pattern]
+                break
+        if wager_name:
+            suffix = {
+                "Over": "O",
+                "Under": "U",
+                "1": "1",
+                "2": "2",
+            }[found_wager.group(1)]
+            cond = float(found_wager.group(2))
+
+            wagers.append(CondWager(wager_name, none_factor, suffix, cond))
+
+    surebet = None
+    if len(wagers) == 2:
+        surebet = Surebet(*wagers)
+    return surebet
