d0f0be87d66c689f8bda04149f6bc090968b0dab
Max <max52853@gmail.com>
2018-01-23 17:42:01 +0300

    Some changes in posit.py for checking...

--- surebet/loading/posit.py
+++ surebet/loading/posit.py
@@ -1,23 +1,36 @@
-from selenium.webdriver.common.keys import Keys
+# from selenium import webdriver
 from surebet.loading import *
 
 
+default_account = {'login' : "kolyan312@gmail.com", 'password' : "1q1w1e1r"}
 name = "positivebet"
-url = "http://positivebet.com/en"
+url = "https://positivebet.com/en/user/login"
+
+node = "#gridBets > table"
 
 
-def log_in(browser, account):
+def load(browser, account = default_account):
     browser.get(url)
-    browser.find_element_by_link_text("Login").send_keys(Keys.RETURN)
     browser.find_element_by_id("UserLogin_username").send_keys(account['login'])
     browser.find_element_by_id("UserLogin_password").send_keys(account['password'])
-    browser.find_element_by_name("yt0").send_keys(Keys.RETURN)
-    browser.find_element_by_link_text("Live bets").send_keys(Keys.RETURN)
-    browser.find_element_by_link_text("Live bets").send_keys(Keys.RETURN)
-    browser.find_element_by_name("yt1").send_keys(Keys.RETURN)
-    browser.find_element_by_id("ddlPerPage").send_keys(Keys.RETURN)
+    browser.find_element_by_id("UserLogin_rememberMe").click()
+    browser.find_element_by_name("yt0").click()
+    browser.find_element_by_link_text("Live bets").click()
+    browser.find_element_by_link_text("Live bets").click()
+    browser.find_element_by_name("yt1").click()
     browser.find_element_by_xpath("//select[@name='Bets[per_page]']/option[text()='30']").click()
     log_loaded(name)
 
 
-# log_in(webdriver.Chrome(), {'login' : "kolyan312@gmail.com", 'password' : "1q1w1e1r"})
+def load_events(browser):
+    result = browser.find_element_by_css_selector(node).get_attribute("outerHTML")
+    log_loaded(name)
+    return result
+
+
+# browser = webdriver.Chrome()
+# browser.get(url)
+# browser.find_element_by_link_text("Live bets").click()
+# print(browser.find_element_by_css_selector(node).get_attribute("outerHTML"))
+#
+# load(browser)
