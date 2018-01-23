854ccb7ef8560628eb2e039acf44bc555a350465
marshygeek <lester0578@gmail.com>
2018-01-23 17:30:56 +0400

    Added positive parsing and testing. Some refactorings (#10)
    * Added positive parsing and testing. Some refactorings
    
    * Added format_spaces after get_text
    
    * Simplified find function

--- surebet/handling/__init__.py
+++ surebet/handling/__init__.py
@@ -12,7 +12,7 @@ class CondWager(Wager):
 
 
 class Surebet:
-    def __init__(self, w1, w2, profit):
+    def __init__(self, w1, w2, profit=None):
         self.w1 = w1
         self.w2 = w2
         self.profit = profit
--- surebet/handling/calculating.py
+++ surebet/handling/calculating.py
@@ -18,29 +18,29 @@ def calc_surebets(bets1, bets2, with_draw=True):
     for bet_name in opposite_bets.keys():
         factor1 = getattr(bets1, bet_name)
         factor2 = getattr(bets2, opposite_bets[bet_name])
-        if check_surebet(factor1, factor2):
+        if _check_surebet(factor1, factor2):
             w1 = Wager(bet_name, factor1)
             w2 = Wager(opposite_bets[bet_name], factor2)
-            surebets.append(Surebet(w1, w2, get_profit(factor1, factor2)))
+            surebets.append(Surebet(w1, w2, _get_profit(factor1, factor2)))
 
-    surebets.extend(handle_cond_bets(bets1, bets2))
+    surebets.extend(_handle_cond_bets(bets1, bets2))
 
     return surebets
 
 
-def check_surebet(factor1, factor2):
+def _check_surebet(factor1, factor2):
     return factor1 and factor2 and (1 / factor1 + 1 / factor2 < 1)
 
 
-def get_profit(factor1, factor2):
+def _get_profit(factor1, factor2):
     profit = (1 - (1 / factor1 + 1 / factor2)) * 100
     return round(profit, 2)
 
 
-def calc_cond_surebet(bet_name, cond_bet1, cond_bet2, bets_reversed):
+def _calc_cond_surebet(bet_name, cond_bet1, cond_bet2, bets_reversed):
     # calculate suffixes (O/U for total or 1/2 for hand)
-    first_suffix = get_bet_suffix(bet_name, 0)
-    second_suffix = get_bet_suffix(bet_name, 1)
+    first_suffix = _get_bet_suffix(bet_name, 0)
+    second_suffix = _get_bet_suffix(bet_name, 1)
 
     # cond is the same for cond_bet1 and cond_bet2
     cond = cond_bet1.cond
@@ -55,10 +55,18 @@ def calc_cond_surebet(bet_name, cond_bet1, cond_bet2, bets_reversed):
     if bets_reversed:
         w1, w2 = w2, w1
 
-    return Surebet(w1, w2, get_profit(cond_bet1.v1, cond_bet2.v2))
+    return Surebet(w1, w2, _get_profit(cond_bet1.v1, cond_bet2.v2))
 
 
-def handle_cond_bets(bets1, bets2):
+def _get_bet_suffix(bet_name, val_num):
+    if bet_name == "hand":
+        suffix = ("1", "2")[val_num]
+    else:
+        suffix = ("O", "U")[val_num]
+    return suffix
+
+
+def _handle_cond_bets(bets1, bets2):
     surebets = []
     for bet_name in ("total", "ind_total1", "ind_total2", "hand"):
         cond_bets2_map = {cond_bet.cond: cond_bet for cond_bet in getattr(bets2, bet_name)}
@@ -66,20 +74,12 @@ def handle_cond_bets(bets1, bets2):
             cond_bet2 = cond_bets2_map.get(cond_bet1.cond, None)
             if cond_bet2:
                 bets_reversed = None
-                if check_surebet(cond_bet1.v1, cond_bet2.v2):
+                if _check_surebet(cond_bet1.v1, cond_bet2.v2):
                     bets_reversed = False
-                elif check_surebet(cond_bet1.v2, cond_bet2.v1):
+                elif _check_surebet(cond_bet1.v2, cond_bet2.v1):
                     bets_reversed = True
 
                 if bets_reversed is not None:
-                    cond_surebet = calc_cond_surebet(bet_name, cond_bet1, cond_bet2, bets_reversed)
+                    cond_surebet = _calc_cond_surebet(bet_name, cond_bet1, cond_bet2, bets_reversed)
                     surebets.append(cond_surebet)
     return surebets
-
-
-def get_bet_suffix(bet_name, val_num):
-    if bet_name == "hand":
-        suffix = ("1", "2")[val_num]
-    else:
-        suffix = ("O", "U")[val_num]
-    return suffix
