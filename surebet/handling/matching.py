e0a7ae64a2cb306cdadb634fb35677c91319a94d
marshygeek <lester0578@gmail.com>
2018-01-24 18:31:11 +0400

    Refactoring

--- surebet/handling/matching.py
+++ surebet/handling/matching.py
@@ -14,15 +14,15 @@ def match_sports(sport1, sport2):
             events = (event1, event2)
             events_teams = _get_teams(events)
 
-            reversed_teams = _match_events(*events)
-            if reversed_teams is not None:
+            is_teams_reversed = _match_events(*events)
+            if is_teams_reversed is not None:
                 used_teams = _get_used_teams(events_teams, matched_teams)
                 if used_teams:
                     _del_used_pair(matched_events, used_teams)
                     continue
 
                 matched_teams.update(events_teams)
-                matched_events.append(MatchedEventPair(*events, reversed_teams))
+                matched_events.append(MatchedEventPair(*events, is_teams_reversed))
 
     return matched_events
 
@@ -32,16 +32,20 @@ def _join_teams(team1, team2):
 
 
 def _match_events(event1, event2):
-    reversed_teams = None
+    is_teams_reversed = None
 
-    teams1 = (event1.team1.lower(), event1.team2.lower())
-    teams2 = (event2.team1.lower(), event2.team2.lower())
+    teams1 = _format_teams(event1)
+    teams2 = _format_teams(event2)
     if _is_equal(_join_teams(*teams1), _join_teams(*teams2)):
-        reversed_teams = False
+        is_teams_reversed = False
     elif _is_equal(_join_teams(*teams1), _join_teams(*reversed(teams2))):
-        reversed_teams = True
+        is_teams_reversed = True
 
-    return reversed_teams
+    return is_teams_reversed
+
+
+def _format_teams(event):
+    return event.team1.lower(), event.team2.lower()
 
 
 def _is_equal(str1, str2):
@@ -65,6 +69,6 @@ def _get_used_teams(events_teams, matched_teams):
 def _del_used_pair(matched_events, used_teams):
     for pair_idx, matched_pair in enumerate(matched_events):
         for event in (matched_pair.event1, matched_pair.event2):
-            if event.team1 == used_teams[0] and event.team2 == used_teams[1]:
+            if (event.team1, event.team2) == (used_teams[0], used_teams[1]):
                 del matched_events[pair_idx]
                 return
