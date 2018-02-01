d864b8e44de537917aef107a552b5f0d67f9479b
korovkinand <korovkinandg@gmail.com>
2018-02-01 20:46:32 +0400

    Rename excluding func. Add posit loading and excluding to integration testing

--- surebet/tests/test_integration.py
+++ surebet/tests/test_integration.py
@@ -4,6 +4,8 @@ import surebet.loading.fonbet as fonbet_loading
 import surebet.loading.marat as marat_loading
 import surebet.parsing.fonbet as fonbet_parsing
 import surebet.parsing.marat as marat_parsing
+from surebet.bookmakers import Posit
+from surebet.handling.excluding import exclude_posit
 from surebet.handling.searching import find_surebets
 from surebet.loading.selenium import Selenium
 from surebet.parsing.bets import Bookmakers
@@ -11,6 +13,8 @@ from surebet.parsing.bets import Bookmakers
 
 def test_integration():
     # loading
+    posit = Posit()
+
     # Fonbet
     selenium = Selenium()
 
@@ -26,6 +30,8 @@ def test_integration():
     fonbet_parsing.parse(fonbet_sample, bookmakers.fonbet)
     marat_parsing.parse(marat_sample, bookmakers.marat)
 
-    find_surebets(bookmakers)
+    found_surebets = find_surebets(bookmakers)
+    posit_surebets = posit.load_events()
+    exclude_posit(found_surebets, posit_surebets)
 
     logging.info("PASS: integration")
