6bfe862b62c8c42172d9017dcf3e1d1f1fad037b
marshygeek <lester0578@gmail.com>
2018-02-02 16:12:25 +0400

    New class SeleniumService: regulates selenium instances

--- surebet/loading/selenium.py
+++ surebet/loading/selenium.py
@@ -3,21 +3,38 @@ import subprocess
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
-# kill Chrome at program launch
-subprocess.run(['killall', '-s', '9', 'Xvfb'])
-
-
-# TODO: remove old files
 
 class Selenium:
     def __init__(self, implicit_wait=60) -> None:
-        self._display = Display(visible=0, size=(1920, 1080))
-        self._display.start()
-
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(implicit_wait)
 
     def quit(self):
         self.browser.quit()
 
-        self._display.stop()
+
+class SeleniumService:
+    _pool = []
+    _display = Display(visible=0, size=(1920, 1080))
+
+    def __init__(self):
+        if not SeleniumService._pool:
+            # kill Chrome at program launch
+            subprocess.run(['killall', '-s', '9', 'Xvfb'])
+            subprocess.run(['killall', '-s', '9', 'chromedriver'])
+
+            # TODO: remove old files
+
+            SeleniumService._display.start()
+
+    @staticmethod
+    def new_instance():
+        SeleniumService._pool.append(Selenium())
+        return SeleniumService._pool[-1]
+
+    @staticmethod
+    def quit():
+        for instance in SeleniumService._pool:
+            instance.quit()
+        SeleniumService._pool.clear()
+        SeleniumService._display.stop()
--- surebet/tests/test_integration.py
+++ surebet/tests/test_integration.py
@@ -5,19 +5,19 @@ import surebet.loading.marat as marat_loading
 import surebet.parsing.fonbet as fonbet_parsing
 import surebet.parsing.marat as marat_parsing
 from surebet.handling.searching import find_surebets
-from surebet.loading.selenium import Selenium
+from surebet.loading.selenium import SeleniumService
 from surebet.parsing.bets import Bookmakers
 
 
 def test_integration():
     # loading
     # Fonbet
-    selenium = Selenium()
+    selenium_service = SeleniumService()
+    selenium = selenium_service.new_instance()
 
     fonbet_loading.load(selenium.browser)
     fonbet_sample = fonbet_loading.load_events(selenium.browser)
 
-    selenium.quit()
     # Marat
     marat_sample = marat_loading.load_events()
 
@@ -27,5 +27,7 @@ def test_integration():
     marat_parsing.parse(marat_sample, bookmakers.marat)
 
     find_surebets(bookmakers)
+
+    selenium_service.quit()
 
     logging.info("PASS: integration")
