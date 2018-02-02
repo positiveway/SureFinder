4de4cfed1253a4fb93e250a16e3de6624a799732
korovkinand <korovkinandg@gmail.com>
2018-02-03 03:20:09 +0400

    Probably fix sudo

--- setup_driver.py
+++ setup_driver.py
@@ -1,18 +1,16 @@
-# sudo required
-
 import subprocess
 import urllib.request
 
 import os
 
 download_dir = os.path.expandvars('$HOME/Downloads')
-unpack_dir = '/usr/bin'
 
-download_path = os.path.join(download_dir, 'chromedriver.zip')
+file_path = os.path.join(download_dir, 'chromedriver.zip')
+unpack_dir = '/usr/bin'
 
 version = '2.35'
 driver_url = 'https://chromedriver.storage.googleapis.com/{}/chromedriver_linux64.zip'.format(version)
 
-urllib.request.urlretrieve(driver_url, download_path)
+urllib.request.urlretrieve(driver_url, file_path)
 
-subprocess.run(['sudo', 'unzip', '-f', download_path, '-d', unpack_dir])
+subprocess.run(['sudo', 'unzip', '-f', file_path, '-d', unpack_dir])
