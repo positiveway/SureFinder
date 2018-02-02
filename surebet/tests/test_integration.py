a3b425504e663b5ef92e81185ef316be9ca5c8ad
korovkinand <korovkinandg@gmail.com>
2018-02-02 19:09:12 +0400

    Posit excluding (#33)
    * Split off surebets to separate file, add equality funcs. Refactoring
    
    * Add CODEOWNERS
    
    * Add excluding of posit's surebets from collected surebets
    
    * Rename excluding func. Add posit loading and excluding to integration testing
    
    * Fix EventSurebet comparison. Add quit to test integration
    
    * Refactoring
    
    * Add TODO format
    
    * FIXME
    
    * Fixed: wrong function call
    
    * Update for selenium service

--- surebet/tests/test_integration.py
+++ surebet/tests/test_integration.py
@@ -4,6 +4,8 @@ import surebet.loading.fonbet as fonbet_loading
 import surebet.loading.marat as marat_loading
 import surebet.parsing.fonbet as fonbet_parsing
 import surebet.parsing.marat as marat_parsing
+from surebet.bookmakers import Posit
+from surebet.handling.excluding import exclude_posit
 from surebet.handling.searching import find_surebets
 from surebet.loading.selenium import SeleniumService
 from surebet.parsing.bets import Bookmakers
@@ -11,14 +13,15 @@ from surebet.parsing.bets import Bookmakers
 
 def test_integration():
     # loading
-    # Fonbet
     sel_service = SeleniumService()
+
+    posit = Posit()
+
     fonbet_sel = sel_service.new_instance()
 
     fonbet_loading.load(fonbet_sel.browser)
     fonbet_sample = fonbet_loading.load_events(fonbet_sel.browser)
 
-    # Marat
     marat_sample = marat_loading.load_events()
 
     bookmakers = Bookmakers()
@@ -26,7 +29,10 @@ def test_integration():
     fonbet_parsing.parse(fonbet_sample, bookmakers.fonbet)
     marat_parsing.parse(marat_sample, bookmakers.marat)
 
-    find_surebets(bookmakers)
+    found_surebets = find_surebets(bookmakers)
+    posit_surebets = posit.load_events()
+
+    exclude_posit(found_surebets, posit_surebets)
 
     sel_service.quit()
 
