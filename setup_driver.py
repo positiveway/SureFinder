caa2b2ef1da745a6f40c4444aba28e42b46ad56f
korovkinand <korovkinandg@gmail.com>
2018-02-03 01:06:13 +0400

    Replace python lib by unzip util

--- setup_driver.py
+++ setup_driver.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
 # sudo required
 
+import subprocess
 import urllib.request
-import zipfile
 
 import os
-import stat
 
 download_dir = os.path.expandvars('$HOME/Downloads')
 unpack_dir = '/usr/bin'
 
 download_path = os.path.join(download_dir, 'chromedriver.zip')
-unpack_path = os.path.join(unpack_dir, 'chromedriver')
 
 version = '2.35'
 driver_url = 'https://chromedriver.storage.googleapis.com/{}/chromedriver_linux64.zip'.format(version)
 
 urllib.request.urlretrieve(driver_url, download_path)
 
-with zipfile.ZipFile(download_path, "r") as zip_ref:
-    zip_ref.extractall(unpack_dir)
-
-st = os.stat(unpack_path)
-os.chmod(unpack_path, st.st_mode | stat.S_IEXEC)
+subprocess.run(['sudo', 'unzip', '-f', download_path, '-d', unpack_dir])
