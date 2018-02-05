ebecb183051406e520525d38dba26218c265cc41
marshygeek <lester0578@gmail.com>
2018-02-05 16:23:29 +0400

    Added classes in bookmakers.py for loading and parsing sites

--- surebet/tests/test_integration.py
+++ surebet/tests/test_integration.py
@@ -1,12 +1,6 @@
 import logging
 
-import surebet.loading.fonbet as fonbet_loading
-import surebet.loading.marat as marat_loading
-import surebet.loading.olimp as olimp_loading
-import surebet.parsing.fonbet as fonbet_parsing
-import surebet.parsing.marat as marat_parsing
-import surebet.parsing.olimp as olimp_parsing
-from surebet.bookmakers import Posit
+from surebet.bookmakers import Posit, Fonbet, Marat, Olimp
 from surebet.handling.excluding import exclude_posit
 from surebet.handling.searching import find_surebets
 from surebet.loading.selenium import SeleniumService
@@ -14,31 +8,21 @@ from surebet.parsing.bets import Bookmakers
 
 
 def test_integration():
-    # loading
-    sel_service = SeleniumService()
-
     posit = Posit()
-
-    fonbet_sel = sel_service.new_instance()
-
-    fonbet_loading.load(fonbet_sel.browser)
-    fonbet_sample = fonbet_loading.load_events(fonbet_sel.browser)
-
-    marat_sample = marat_loading.load_events()
-
-    olimp_sample = olimp_loading.load_events()
+    fonbet = Fonbet()
+    marat = Marat()
+    olimp = Olimp()
 
     bookmakers = Bookmakers()
-    # parsing
-    fonbet_parsing.parse(fonbet_sample, bookmakers.fonbet)
-    marat_parsing.parse(marat_sample, bookmakers.marat)
-    olimp_parsing.parse(olimp_sample, bookmakers.olimp)
+    fonbet.load_events(bookmakers.fonbet)
+    marat.load_events(bookmakers.marat)
+    olimp.load_events(bookmakers.olimp)
 
     found_surebets = find_surebets(bookmakers)
     posit_surebets = posit.load_events()
 
     exclude_posit(found_surebets, posit_surebets)
 
-    sel_service.quit()
+    SeleniumService.quit()
 
     logging.info("PASS: integration")
