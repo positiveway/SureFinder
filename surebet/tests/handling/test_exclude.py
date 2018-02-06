6aa48360356855d7be8f946745f9a958b6b85df6
Unknown <vane_oook@mail.ru>
2018-02-06 20:39:37 +0400

    Optimized testing
    Combine input _other and _posit surebets in 1 pickle file. Known_result test now has json-integration and check equality deeper

--- surebet/tests/handling/test_exclude.py
+++ surebet/tests/handling/test_exclude.py
@@ -1,44 +1,45 @@
 import logging
 import pickle
-
+import json
 from os import path
 
+from surebet.json_funcs import json_dumps, obj_dumps
 from surebet.tests.handling import package_dir
-
 from surebet.handling.excluding import exclude_posit
 
 resource_dir = path.join(package_dir, "excluding")
 
 
 def read_sample(filename):
-    found_file = open(path.join(resource_dir, filename), "rb")
-    return pickle.load(found_file)
+    with open(path.join(resource_dir, filename), "rb") as found_file:
+        obj = pickle.load(found_file)
+    return obj[0], obj[1]
+
+
+def surebets_to_json(surebets):
+    for books in surebets.books_surebets:
+        ad = books.attrs_dict()
+        for sport in ad:
+            for match in ad[sport]:
+                match.teams1 = list(match.teams1)
+                match.teams2 = list(match.teams2)
+    return obj_dumps(surebets.books_surebets)
 
 
 def test_known_result():
-    other_surebets = read_sample('known_other.pkl')
-    posit_surebets = read_sample('known_posit.pkl')
-    result_surebets = read_sample('known_result.pkl')
+    surebets_other, surebets_posit = read_sample("known.pkl")
+    exclude_posit(surebets_other, surebets_posit)
 
-    exclude_posit(other_surebets, posit_surebets)
+    with open(path.join(resource_dir, "known_result.json"), "r") as file_result:
+        surebets_result = json.load(file_result)
 
-    # compare above-computed result with known result
-    assert len(other_surebets.books_surebets) == len(result_surebets.books_surebets)
-    for os, rs in zip(other_surebets.books_surebets, result_surebets.books_surebets):
-        # compare BookSurebets
-        assert os == rs
-        # compare EventSurebets in BookLevel attributes' list
-        ad_os = os.attrs_dict()
-        ad_rs = rs.attrs_dict()
-        assert sum(1 for sport in ad_os if ad_os[sport] == ad_rs[sport])
+    assert surebets_to_json(surebets_other) == json_dumps(surebets_result)
 
-    logging.info("PASS: excluding known result")
+    logging.info("PASS: known")
 
 
 def test_sample():
-    for i in range(2):
-        other_surebets = read_sample('sample{0}_other.pkl'.format(i))
-        posit_surebets = read_sample('sample{0}_posit.pkl'.format(i))
-        exclude_posit(other_surebets, posit_surebets)
+    for i in range(3):
+        exclude_posit(*read_sample('sample{}.pkl'.format(i)))
 
-    logging.info("PASS: excluding samples")
+    logging.info("PASS: samples")
