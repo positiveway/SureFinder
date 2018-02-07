03fc6ccb0ff32fb959cc96d297bb3fa398b79978
korovkinand <korovkinandg@gmail.com>
2018-02-07 13:59:45 +0400

    Update chrome_install.sh

--- chrome_install.sh
+++ chrome_install.sh
@@ -1,4 +1,1 @@
-if [ ! -f $HOME/Downloads/chrome.zip ]; then
-  wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P $HOME/Downloads -O chrome.zip
-  sudo unzip $HOME/Downloads/chrome.zip -d /usr/bin
-fi
+
