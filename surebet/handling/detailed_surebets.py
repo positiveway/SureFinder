f388426dac669316f5d135a63ba17044a9d116ab
marshygeek <lester0578@gmail.com>
2018-02-09 17:49:33 +0400

    Updated main.py

--- surebet/handling/detailed_surebets.py
+++ surebet/handling/detailed_surebets.py
@@ -1,24 +1,26 @@
 from functools import cmp_to_key
 from surebet.handling.surebets import *
 
-PROFIT_LOW_LIM = 1
-PROFIT_UP_LIM = 10
+MIN_PROFIT = 2.5
+MAX_PROFIT = 10
 
-LIFETIME_LOW_LIM = 15
+MIN_LIFETIME = 10
+
+TEAMS_SEPARATOR = " vs "
 
 
 class DetailedSurebet:
     """
-    Contain all information about surebet: bookmakers between which surebet is appeared,
-    teams for first and second event, part of event and wagers, profit, life time of surebet
+    Contain all information about surebet: bookmakers between which surebet is occurred,
+    teams for first and second event, part of event and wagers, profit, lifetime of surebet
     """
 
     def __init__(self, books: BookSurebets, sport: str, events: EventSurebets, part: PartSurebets,
                  surebet: TimedSurebet):
         """
-        :param books: bookmakers between which surebet is appeared
+        :param books: bookmakers between which surebet is occurred
         :param sport: name of sport
-        :param events: events between which surebet is appeared
+        :param events: events between which surebet is occurred
         :param part: common part of events
         """
         self.book1, self.book2 = books.book1, books.book2
@@ -27,16 +29,16 @@ class DetailedSurebet:
         self.part = part.part
         self.w1, self.w2 = surebet.w1, surebet.w2
         self.profit = surebet.profit
-        self.life_time = surebet.get_lifetime()
+        self.lifetime = surebet.get_lifetime()
 
     def __str__(self):
-        event_name1 = " vs ".join(self.teams1)
-        event_name2 = " vs ".join(self.teams2)
+        event_name1 = TEAMS_SEPARATOR.join(self.teams1)
+        event_name2 = TEAMS_SEPARATOR.join(self.teams2)
         str_form = "profit: {profit:<8} | {sport:<6} | {book1:<6} | {ev_name1:<60} | part #{part} | {w1}\n".format(
             profit=self.profit, book1=self.book1, sport=self.sport, ev_name1=event_name1, part=self.part,
             w1=self.w1)
         str_form += "lifetime: {time:<6} | {sport:<6} | {book2:<6} | {ev_name2:<60} | part #{part} | {w2}\n".format(
-            time=self.life_time, book2=self.book2, sport=self.sport, ev_name2=event_name2, part=self.part,
+            time=self.lifetime, book2=self.book2, sport=self.sport, ev_name2=event_name2, part=self.part,
             w2=self.w2)
         return str_form
 
@@ -53,8 +55,8 @@ def convert_to_detailed(surebets: Surebets):
 
 
 def _filter(detailed_surebets):
-    """Filter detailed_surebets by profit's lower and upper limits and by life_time's lower limit"""
-    return filter(lambda item: PROFIT_LOW_LIM <= item.profit <= PROFIT_UP_LIM and LIFETIME_LOW_LIM <= item.life_time,
+    """Filter detailed_surebets by profit's lower and upper limits and by lifetime's lower limit"""
+    return filter(lambda item: MIN_PROFIT <= item.profit <= MAX_PROFIT and MIN_LIFETIME <= item.lifetime,
                   detailed_surebets)
 
 
@@ -71,11 +73,11 @@ def _convert_to_detailed(surebets: Surebets):
 
 
 def _detailed_cmp(first: DetailedSurebet, second: DetailedSurebet):
-    """Custom comparator to sort detailed_surebets firstly by profit casted to int and secondly by life_time"""
+    """Custom comparator to sort detailed_surebets firstly by profit casted to int and secondly by lifetime"""
     first_profit, second_profit = int(first.profit), int(second.profit)
     if first_profit < second_profit:
         return -1
     elif first_profit == second_profit:
-        return first.life_time - second.life_time
+        return first.lifetime - second.lifetime
     else:
         return 1
