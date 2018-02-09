f9bcdd06e933a3a962dd9b6cf6ad7b4204446491
Unknown <vane_oook@mail.ru>
2018-02-09 14:26:52 +0400

    Mocking test added. Need to generate jsons now

--- surebet/tests/loading/test_posit.py
+++ surebet/tests/loading/test_posit.py
@@ -4,6 +4,7 @@ import json
 from unittest.mock import patch
 from requests import Session
 
+from surebet.tests import SurebetsJSONDecoder
 from surebet.json_funcs import json_dumps, obj_dumps
 from surebet.loading.posit import *
 from surebet.tests.loading import check_result, package_dir
@@ -64,7 +65,7 @@ def test_sample():
 
 
 def test_known_result():
-    with open(path.join(resource_dir, "known.json"), "r") as file_known:
+    with open(path.join(resource_dir, "known.json")) as file_known:
         sample_known = json.load(file_known)
     surebets_posit = mock_load_events(sample_known)
 
@@ -74,3 +75,48 @@ def test_known_result():
     assert obj_dumps(surebets_posit) == json_dumps(surebets_known)
 
     logging.info("PASS: known result")
+
+
+def mock_posit():
+    with patch("surebet.bookmakers.try_load") as mock_try_load:
+        mock_try_load.return_value = ""  # needed for first try_load call in Posit's constructor
+        with patch('surebet.bookmakers.Posit._add_new_surebets') as mock_add:
+            mock_add.return_value = 0
+            result = Posit()
+
+    return result
+
+
+def mock_decrease_marks(posit):
+    filename = path.join(resource_dir, 'knownDecreaseLoads.json')
+    with open(filename) as file_loads:
+        list_loads = json.load(file_loads)
+
+    iter_loads = iter(list_loads)
+
+    def load_next_html(load_func, site_name, **kwargs):  # it's called instead of try_load
+        try:
+            sample_next = next(iter_loads)
+        except StopIteration:
+            raise AssertionError("Sample requests overflow") from StopIteration
+
+        return sample_next
+
+    with patch("surebet.bookmakers.try_load") as mock_try_load:
+        mock_try_load.side_effect = load_next_html()
+        for load_idx in range(len(list_loads)):
+            posit.load_events()
+
+
+def test_decrease_known():
+    with open(path.join(resource_dir, 'knownDecrease.json')) as file_known:
+        surebets = json.load(file_known, cls=SurebetsJSONDecoder)
+
+    posit = mock_posit()
+    posit.surebets = surebets
+    mock_decrease_marks(posit)
+
+    with open(path.join(resource_dir, 'knownDecreaseResult.json')) as file_known_result:
+        surebets_known = json.load(file_known_result)
+
+    assert obj_dumps(posit.surebets) == json_dumps(surebets_known)
