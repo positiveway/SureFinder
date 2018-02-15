6415d294269865e7d6fe5d35eb495f4fd3b7db99
marshygeek <lester0578@gmail.com>
2018-02-15 20:04:40 +0400

    FilterUnnecessaryEvents (#102)
    * Added func exclude_unpopular: removes unnecessary events that was
    referred in issue #87
    
    * Less patterns
    
    * Redundant parameter "r"

--- surebet/tests/handling/test_unpopular.py
+++ surebet/tests/handling/test_unpopular.py
@@ -5,33 +5,33 @@ from os import path
 
 from surebet.tests.handling import package_dir
 from surebet.json_funcs import json_dumps, obj_dumps
-from surebet.handling.excluding import exclude_posit
+from surebet.handling.excluding import exclude_unpopular
 
-resource_dir = path.join(package_dir, "excluding")
+resource_dir = path.join(package_dir, "unpopular")
 
 
 def read_sample(filename):
     with open(path.join(resource_dir, filename), "rb") as found_file:
         obj = pickle.load(found_file)
-    return obj[0], obj[1]
+    return obj
 
 
 def test_sample():
     for sample_num in range(3):
         filename = 'sample{}.pkl'.format(sample_num)
-        surebets_other, surebets_posit = read_sample(filename)
-        exclude_posit(surebets_other, surebets_posit)
+        surebets = read_sample(filename)
+        exclude_unpopular(surebets)
 
     logging.info("PASS: samples")
 
 
 def test_known_result():
-    surebets_other, surebets_posit = read_sample("known.pkl")
-    exclude_posit(surebets_other, surebets_posit)
+    surebets = read_sample("known.pkl")
+    exclude_unpopular(surebets)
 
     with open(path.join(resource_dir, "knownResult.json")) as file_result:
         surebets_result = json.load(file_result)
 
-    assert obj_dumps(surebets_other) == json_dumps(surebets_result)
+    assert obj_dumps(surebets) == json_dumps(surebets_result)
 
     logging.info("PASS: known")
