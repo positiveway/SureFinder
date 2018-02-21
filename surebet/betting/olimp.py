b955d141ebf55b4ee9f12f6afeb74529e34156a1
marshygeek <lester0578@gmail.com>
2018-02-21 12:52:37 +0400

    Created class OlimpBot. Moved funcs in OlimpBot

--- surebet/betting/olimp.py
+++ surebet/betting/olimp.py
@@ -3,54 +3,56 @@ from surebet.loading import LoadException
 
 import logging
 
-login = "3959858"
-passw = "O335673J"
-
-session_payload = base_payload.copy()
-
-
-def _init():
-    req_url = base_url.format("autorize")
-
-    payload = base_payload.copy()
-    payload.update({
-        "login": login,
-        "passw": passw,
-    })
-
-    resp = requests.post(req_url, headers=get_xtoken(payload), data=payload)
-    check_status(resp.status_code)
-
-    session_payload["session"] = resp.json()["data"]["session"]
+default_account = {
+    "login": "3959858",
+    "passw": "O335673J",
+}
 
 
-_init()
+class OlimpBot:
+    """Use to place bets on olimp site."""
 
-factor_id = "906824311:38209913:3:7:2.5:1:0:0:1"
-sport_id = 1
-sum = 10
-factor = 1.17
+    def __init__(self, account: dict = default_account) -> None:
+        self.session_payload = base_payload.copy()
 
+        self.sign_in(account)
 
-def place_bet():
-    url = base_url.format("basket/fast")
+    def sign_in(self, account: dict) -> None:
+        """Sign in to olimp, remember session id."""
+        req_url = base_url.format("autorize")
 
-    payload = session_payload.copy()
-    payload.update({
-        "coefs_ids": '[["{apid}",{factor},1]]'.format(apid=factor_id, factor=factor),
-        "sport_id": sport_id,
-        "sum": sum,
-        "save_any": 1,
-        "fast": 1,
-    })
+        payload = base_payload.copy()
+        payload.update(account)
 
-    headers = base_headers.copy()
-    headers.update(get_xtoken(payload))
+        resp = requests.post(req_url, headers=get_xtoken(payload), data=payload)
+        check_status(resp.status_code)
 
-    resp = requests.post(url, headers=headers, data=payload)
-    print(resp.text)
-    check_status(resp.status_code)
-    res = resp.json()
-    if "data" not in res or res["data"] != "Your bet is successfully accepted!":
-        logging.info(res)
-        raise LoadException("error while placing the bet")
+        self.session_payload["session"] = resp.json()["data"]["session"]
+
+    def place_bet(self, amount: int, wager) -> None:
+        """
+        :param amount: amount of money to be placed (RUB)
+        :param wager: defines on which wager bet is to be placed (OlimpWager or OlimpCondWager)
+        """
+        olimp_info = wager.olimp_info
+
+        url = base_url.format("basket/fast")
+
+        payload = self.session_payload.copy()
+        payload.update({
+            "coefs_ids": '[["{apid}",{factor},1]]'.format(apid=olimp_info.factor_id, factor=wager.factor),
+            "sport_id": olimp_info.sport_id,
+            "sum": amount,
+            "save_any": 1,
+            "fast": 1,
+        })
+
+        headers = base_headers.copy()
+        headers.update(get_xtoken(payload))
+
+        resp = requests.post(url, headers=headers, data=payload)
+        check_status(resp.status_code)
+        res = resp.json()
+        if "data" not in res or res["data"] != "Your bet is successfully accepted!":
+            logging.error(res)
+            raise LoadException("error while placing the bet")
