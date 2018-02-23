ff0d94b24c4da56488f790cc4adbdd88fac65ad9
Aslan Dukaev <dukaev999@gmail.com>
2018-02-23 13:49:12 +0300

    Add front

---
+++ surebet/ui/front-vue/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
